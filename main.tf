25d630d7c87a3677a73163de12da0322f28b3d61
BalaAnkiReddy-Jinkala <jinkalaankireddy27@gmail.com>
2025-03-19 19:42:04 +0530

    commit

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-b83"
+    key    = "tools/state"
+    region = "us-east-1"
+  }
+}
+
+variable "ami_id" {
+  default = "ami-08b5b3a93ed654d19"
+}
+
+variable "zone_id" {
+  default = "Z0309342AAI7YO674JQ1"
+}
+
+variable "tools" {
+  default = {
+
+    vault = {
+      instance_type = "t3.small"
+      port          = 8200
+    }
+
+  }
+}
+
+
+module "tool-infra" {
+  source   = "./module-infra"
+  for_each = var.tools
+
+  ami_id        = var.ami_id
+  instance_type = each.value["instance_type"]
+  name          = each.key
+  port          = each.value["port"]
+  zone_id       = var.zone_id
+}
