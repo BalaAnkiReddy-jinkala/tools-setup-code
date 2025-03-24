351b43aac489ea69023a3c655201d2e43a1d16db
BalaAnkiReddy-Jinkala <jinkalaankireddy27@gmail.com>
2025-03-24 16:57:57 +0530

    commit

---
+++ misc/vault-secrets/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-b83"
+    key    = "vault-secrets/state"
+    region = "us-east-1"
+  }
+}
+
+
+provider "vault" {
+  address = "http://vault-internal.ankidevops.online:8200"
+  token = var.vault_token
+}
+
+variable "vault_token" {}
+
+resource "vault_mount" "ssh" {
+  path        = "infra"
+  type        = "kv"
+  options     = { version = "2" }
+  description = "Infra secrets"
+}
+
+resource "vault_generic_secret" "ssh" {
+  path = "${vault_mount.ssh.path}/ssh"
+
+  data_json = <<EOT
+{
+  "username":   "ec2-user",
+  "password": "DevOps321"
+}
+EOT
+}
+
